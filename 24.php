2f18c25b7fbb91b7039391b8312d3edfe2c9916d
Tsaqibbbb <muhammad_shaqr_ts7_24@student.smktelkom.sda.sch.id>
2024-11-29 10:35:28 +0700

    first commit

---
+++ 24.php
@@ -0,0 +1,44 @@
+<?php
+$a = 18;
+$b = 4;
+//Menjumlah variabel a dan variabel b
+echo "Hasil dari $a + $b =" . $hasil = $a+$b;
+echo "<br>";
+//Mengurangkan variabel a dan variabel b
+echo "Hasil dari $a - $b =" . $hasil = $a-$b;
+echo "<br>";
+//Mengalikan variabel a dan variabel b
+echo "Hasil dari $a * $b =" . $hasil = $a*$b;
+echo "<br>";
+//Membagikan variabel a dan variabel b
+echo "Hasil dari $a / $b =" . $hasil = $a/$b;
+echo "<br>";
+echo "<br>";
+echo "Bagian operator perbandingan";
+ $x = 24;
+ $y = 2;
+ echo "<br>";
+echo "x = 24 <br>";
+echo "y = 4 <br>";
+//membandingkan variabel x dengan variabel y
+echo "<br>";
+$lebih_besar = $x > $y;
+echo "apakah $x lebih besar dari $y? ". $lebih_besar ;
+echo"<br>";
+$lebih_kecil = $x < $y;
+echo "apakah $x lebih kecil dari $y?". $lebih_kecil;
+echo "<br>";
+echo "<br>";
+$v = 6;
+$w = 4;
+//membandingkan variabel v dengan variabel w
+echo "v = 6 <br>";
+echo "w = 4 <br>";
+echo "<br>";
+$sama_dengan = $v == $w; 
+echo "apakah $v sama dengan $w?". $sama_dengan;
+echo "<br>";
+$tidak_sama_dengan = $v != $w;
+echo "apakah $v tidak sama dengan $w?". $tidak_sama_dengan;
+
+?>
